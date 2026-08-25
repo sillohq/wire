3a5670bf205b64c9cd22c297a5ec404c53683428
chidebele dunamis <techwithdunamix@gmail.com>
2026-08-25 06:41:35 +0100

    Export Peer

--- sillo/wire/__init__.py
+++ sillo/wire/__init__.py
@@ -1,5 +1,6 @@
 from sillo.wire.envelope import DeliveryReport, Encoding, Envelope
 from sillo.wire.errors import PeerGone, RoomNotFound, WireError
+from sillo.wire.peer import Peer
 from sillo.wire.policy import Overflow
 
 __version__ = "0.1.0"
@@ -9,6 +10,7 @@ __all__ = [
     "Encoding",
     "Envelope",
     "Overflow",
+    "Peer",
     "PeerGone",
     "RoomNotFound",
     "WireError",
