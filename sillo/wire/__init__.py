6734881c456c04b8e859ebbb2f034531dff43245
chidebele dunamis <techwithdunamix@gmail.com>
2026-08-26 09:10:10 +0100

    Export the backlog types

--- sillo/wire/__init__.py
+++ sillo/wire/__init__.py
@@ -1,3 +1,4 @@
+from sillo.wire.backlog import Backlog, MemoryBacklog, NullBacklog
 from sillo.wire.envelope import DeliveryReport, Encoding, Envelope
 from sillo.wire.errors import PeerGone, RoomNotFound, WireError
 from sillo.wire.peer import Peer
@@ -6,9 +7,12 @@ from sillo.wire.policy import Overflow
 __version__ = "0.1.0"
 
 __all__ = [
+    "Backlog",
     "DeliveryReport",
     "Encoding",
     "Envelope",
+    "MemoryBacklog",
+    "NullBacklog",
     "Overflow",
     "Peer",
     "PeerGone",
